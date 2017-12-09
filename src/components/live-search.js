8cdad5d19433d1560983dece04635cb90d2aa71c
pedrom40 <pedro.morin1980@gmail.com>
2017-12-09 10:39:39 -0600

    completed exercise

--- src/components/live-search.js
+++ src/components/live-search.js
@@ -3,11 +3,34 @@ import React from 'react';
 import SearchForm from './search-form';
 import CharacterList from './character-list';
 
-export default function LiveSearch(props) {
-  return (
-    <div className="live-search">
-      <SearchForm />
-      <CharacterList characters={props.characters} />
-    </div>
-  );
+export default class LiveSearch extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      characters: this.props.characters
+    }
+  };
+
+  setSearchTerm(term) {
+
+    let characters = [];
+
+    this.props.characters.map( character => {
+      if (character.name.search(term) !== -1) {
+        return characters.push(character);
+      }
+    });
+
+    this.setState({
+      characters: characters
+    })
+  };
+
+  render() {
+    return  <div className="live-search">
+              <SearchForm onChange={value => this.setSearchTerm(value)}/>
+              <CharacterList characters={this.state.characters}/>
+            </div>
+  };
 }
